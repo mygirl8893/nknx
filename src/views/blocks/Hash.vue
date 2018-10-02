c6eeaed2ee97df9beb7a87f8410f638a7e87383b
Christian Busch <kontakt@christian-busch.com>
2018-10-02 18:39:21 +0200

    Adjusted axios queries to new nkn-explorer
    Signed-off-by: Christian Busch <kontakt@christian-busch.com>

--- src/views/blocks/Hash.vue
+++ src/views/blocks/Hash.vue
@@ -64,7 +64,7 @@ export default {
   mounted() {
       self = this;
       //Call to NKN-API https://github.com/CrackDavid/nkn-api
-      axios.get("http://nknx.org/api/blocks/"+this.$route.params.hash+"/flat").then(function(response){
+      axios.get("http://nknx.org/api/blocks/"+this.$route.params.hash).then(function(response){
           self.items = response.data[0];
           self.loader=false;
       });
@@ -74,7 +74,7 @@ export default {
       this.loader=true;
       self = this;
       //Call to NKN-API https://github.com/CrackDavid/nkn-api
-      axios.get("http://nknx.org/api/blocks/"+this.$route.params.hash+"/flat").then(function(response){
+      axios.get("http://nknx.org/api/blocks/"+this.$route.params.hash).then(function(response){
           self.items = response.data[0];
           self.loader=false;
       });

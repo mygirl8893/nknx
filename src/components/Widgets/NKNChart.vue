e99401b55816f30699aa1239d32d85f78233ee34
Andrey <andrtaw1@gmail.com>
2018-10-12 16:07:39 +0300

    add: market stats

--- src/components/Widgets/NKNChart.vue
+++ src/components/Widgets/NKNChart.vue
@@ -2,12 +2,30 @@
    <div class="mt-4 visitor-area-chart">
 		<div class="d-custom-flex justify-space-between px-4 mb-4 label-wrap">
 			<h4>Market</h4>
-			<div class="d-custom-flex justify-space-between w-30">
-				<div class="w-50">
+			<div class="d-custom-flex w-50 w-xs-full">
+				<div class="w-20 d-md-half-block w-xs-full">
+					<h4 class="green--text mb-0">${{nknCap}}M</h4>
+					<p class="fs-12 grey--text mb-0">Marketcap</p>
+				</div>
+				<div class="w-20 d-md-half-block w-xs-full">
+					<h4 class="green--text mb-0">${{nknVolume}}K</h4>
+					<p class="fs-12 grey--text mb-0">Daily Bolume</p>
+				</div>
+				<div class="w-20 d-md-half-block w-xs-full">
+					<h4 class="green--text mb-0">#{{nknRank}}</h4>
+					<p class="fs-12 grey--text mb-0">Market Rank</p>
+				</div>
+				<div class="w-20 d-md-half-block w-xs-full">
+					<h4 class="green--text mb-0">{{nkn24}}%</h4>
+					<p class="fs-12 grey--text mb-0">Change 24 hours</p>
+				</div>
+			</div>
+			<div class="d-custom-flex w-20 w-xs-full">
+				<div class="w-50 d-md-full w-xs-full">
 					<h4 class="secondary--text mb-0">ETH {{nknETH}}</h4>
 					<p class="fs-12 grey--text mb-0">NKN/ETH</p>
 				</div>
-				<div class="w-50">
+				<div class="w-50 d-md-full w-xs-full">
 					<h4 class="primary--text mb-0">$ {{nknUSD}}</h4>
 					<p class="fs-12 grey--text mb-0">NKN/USD</p>
 				</div>
@@ -38,6 +56,15 @@ export default {
 				self.nknETH  = 'error';
 			})
 
+			axios.get('https://api.coinmarketcap.com/v2/ticker/2780/')
+                .then(response => {
+                    self.nknRank = response.data.data.rank
+                    self.nknCap = ((response.data.data.quotes.USD.market_cap) / 1000000).toFixed(2)
+                    self.nknVolume = ((response.data.data.quotes.USD.volume_24h) / 1000).toFixed(2)
+                    self.nkn24 = response.data.data.quotes.USD.percent_change_24h
+                    self.nknWeekly = response.data.data.quotes.USD.percent_change_7d
+                })
+
 		}
 	},
 	destroyed () {
@@ -55,6 +82,11 @@ export default {
 			interval:null,
 			nknUSD: 0,
 			nknETH: 0,
+			nknRank: 0,
+			nknCap: 0,
+			nknVolume: 0,
+			nkn24: 0,
+			nknWeekly: 0
 		}
 	}
 };

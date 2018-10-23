f2234acddcf782c0c87421470233eba3a00f4e6e
Andrey <andrtaw1@gmail.com>
2018-10-23 14:29:25 +0300

    add: map refresher

--- src/components/Widgets/Map.vue
+++ src/components/Widgets/Map.vue
@@ -14,42 +14,53 @@ import axios from "axios";
 export default {
   data(){
   	return {
+    interval: null, 
 		loader: true,
 		crawlCounter: 'Loading...'
   	}
   },
+  destroyed() {
+        clearInterval(this.interval);
+  },
   created: function(){
-  	const self = this;
-	axios.get('https://nknx.org/api/crawledNodes', {})
-    .then((response) => {
-        self.crawlCounter = response.data.length
-    }) 
+  	this.getMap()
   },
   mounted() {
-  	const self = this;
-  	let nodes = []
-  	let countryNodeList= {}
-  	axios.get('crawledNodes?withLocation=true').then(function(nodeList){
+    this.interval = setInterval(this.getMap, 30000);
+  },
+  methods: {
+    getMap(){
+      $("#world-map").empty()
+      const self = this;
+      self.loader = true
+      axios.get('https://nknx.org/api/crawledNodes', {})
+      .then((response) => {
+          self.crawlCounter = response.data.length
+      })  
+
+      let nodes = []
+      let countryNodeList= {}
+      axios.get('crawledNodes?withLocation=true').then(function(nodeList){
         nodeList.data.forEach(function(node) {
-        	let regionMarker = ''
-        	if(node.city != null){
-        		regionMarker = node.city
-        	} else if (node.region_name != null){
-        		regionMarker = node.region_name
-        	} else if(node.country_name != null){
-        		regionMarker = node.country_name
-        	} else{
-        		regionMarker = '???'
-        	}
-        	//Nodes in country Counter
-        	if(node.country_code != null){
-				let countryCode = node.country_code
-        			if(countryNodeList.hasOwnProperty(countryCode)){
-        				countryNodeList[countryCode] ++
-        			} else{
-        				countryNodeList[countryCode] = 1
-        			}
-        	}
+          let regionMarker = ''
+          if(node.city != null){
+            regionMarker = node.city
+          } else if (node.region_name != null){
+            regionMarker = node.region_name
+          } else if(node.country_name != null){
+            regionMarker = node.country_name
+          } else{
+            regionMarker = '???'
+          }
+          //Nodes in country Counter
+          if(node.country_code != null){
+        let countryCode = node.country_code
+              if(countryNodeList.hasOwnProperty(countryCode)){
+                countryNodeList[countryCode] ++
+              } else{
+                countryNodeList[countryCode] = 1
+              }
+          }
            nodes.push({latLng: [node.latitude, node.longitude], name: regionMarker})
            
         });
@@ -76,15 +87,15 @@ export default {
         backgroundColor: ChartConfig.color.white,
         markers: nodes,
         series: {
-        	regions: [{
-          		scale: ['#c4d9ec', '#08519C'],
-          		attribute: 'fill',
-          		values: countryNodeList
-        	}]
+          regions: [{
+              scale: ['#c4d9ec', '#08519C'],
+              attribute: 'fill',
+              values: countryNodeList
+          }]
       },
         onRegionTipShow: function(event, label, index){
           if(countryNodeList[index] === undefined){
-          	countryNodeList[index] = 0
+            countryNodeList[index] = 0
           }
           label.html(
           label.html()+'</br>'+
@@ -93,7 +104,9 @@ export default {
       }
       });
       self.loader= false
-    };	
+    };  
+
+    }
   }
 };
 </script>

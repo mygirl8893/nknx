ed12e1eb3022d53326d1050aefba5f9994e4b47b
Andrey <andrtaw1@gmail.com>
2018-12-21 13:54:17 +0300

    lang: add chinese

--- src/store/modules/settings/data.js
+++ src/store/modules/settings/data.js
@@ -1,68 +1,68 @@
 export const languages = [
-   {
-      name: "English",
-      icon: "en",
-      locale: "en"
-   },
-   {
-      name: "Deutsch",
-      icon: "de",
-      locale: "de"
-   },
-   {
+  {
+    name: "English",
+    icon: "en",
+    locale: "en"
+  },
+  {
+    name: "Deutsch",
+    icon: "de",
+    locale: "de"
+  },
+  {
     name: "Русский",
     icon: "ru",
     locale: "ru"
-   },
-   {
+  },
+  {
     name: "日本語",
     icon: "ja",
     locale: "ja"
-   },
-   {
-    name: "한글",
-    icon: "ko",
-    locale: "ko"
-   },
-   {
-    name: "Nederlands",
-    icon: "nl",
-    locale: "nl"
-   },
-   {
-    name: "Türkce",
-    icon: "tr",
-    locale: "tr"
-   },
-   {
+  },
+  {
     name: "简体中文",
     icon: "zh",
     locale: "zh"
-   },
-   {
+  },
+  {
+    name: "한글",
+    icon: "ko",
+    locale: "ko"
+  },
+  {
+    name: "Nederlands",
+    icon: "nl",
+    locale: "nl"
+  },
+  {
+    name: "Türkce",
+    icon: "tr",
+    locale: "tr"
+  },
+  {
     name: "Suomi",
     icon: "fi",
     locale: "fi"
-   },
-   {
+  },
+  {
     name: "Español",
     icon: "es",
     locale: "es"
-   },
-   {
+  },
+  {
     name: "Română",
     icon: "ro",
     locale: "ro"
-   },
-   {
+  },
+  {
     name: "Português",
     icon: "pt",
     locale: "pt"
-   },
-   {
+  },
+  {
     name: "česky",
     icon: "cs",
     locale: "cs"
-   },
+  },
 ]
 

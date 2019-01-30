e1daa785bb29e1f048a53636be49bdb7b1053fb3
Andrey <andrtaw1@gmail.com>
2019-01-30 22:00:48 +0300

    feat: add japanese translator

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -106,8 +106,12 @@ export default {
           icon: "zh",
           language: "简体中文",
           name: "Bruce"
+        },
+        {
+          icon: "ja",
+          language: "日本語",
+          name: "katakoto"
         }
-
       ]
 
       
